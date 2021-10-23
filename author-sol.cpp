1e1a2af8711ad0f978aa5bc496c232bec1943142
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-10-23 14:16:56 +0800

    Update author-sol.cpp

--- author-sol.cpp
+++ author-sol.cpp
@@ -14,7 +14,7 @@ int main() {
 	// A more intelligent solution is to sort the four numbers and pair up the 
 	// first 2 together and the last 2 together and check their ratios. This works 
 	// because ratios are symmetric.
-	// For checking the ratio equality, we can use an simple property that 
+	// For checking the ratio equality, we can use a simple property that 
 	// if a:b = c:d then a*d = b*c. This way, we can avoid dealing with floats.
 	while (next_permutation(perm, perm + 4)) {
 		if (num[perm[0]] * num[perm[3]] == num[perm[1]] * num[perm[2]]) {
